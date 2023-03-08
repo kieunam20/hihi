b4257f75061645d2c4a7dcdb0eceaa948def0dab
kieunam20 <109325427+kieunam20@users.noreply.github.com>
2023-03-08 11:23:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    title: 'Nếu giọt nước là những nụ hôn, anh sẽ trao em biển cả. Nếu lá là những ôm ấp vuốt ve, anh sẽ tặng em cả rừng cây. Nếu đêm dài là tình yêu, anh muốn gửi em cả trời sao lấp lánh.😙',
+    desc: 'Nhưng trái tim anh không thể dành tặng em được vì nơi đó đã thuộc về em rồi. Chúc em một ngày 8/3 hạnh phúc. ',
+    btnYes: 'Yêu anh <33',
+    btnNo: 'Iu anh nhìu hơn <33',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu 😘😘 <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: '5h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://m.me/namkieu.git' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/hoangnamkieu.22/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
