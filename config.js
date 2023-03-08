b81c7ea0d1650128ccfbb9392aa601c01c5e654d
kieunam20 <109325427+kieunam20@users.noreply.github.com>
2023-03-08 11:31:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Dương à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Nếu giọt nước là những nụ hôn, anh sẽ trao em biển cả. Nếu lá là những ôm ấp vuốt ve, anh sẽ tặng em cả rừng cây. Nếu đêm dài là tình yêu, anh muốn gửi em cả trời sao lấp lánh.😙',
-    desc: 'Nhưng trái tim anh không thể dành tặng em được vì nơi đó đã thuộc về em rồi. Chúc em một ngày 8/3 hạnh phúc. ',
+    title: 'Gửi em yêu của anh',
+    desc: 'Nếu giọt nước là những nụ hôn, anh sẽ trao em biển cả. Nếu lá là những ôm ấp vuốt ve, anh sẽ tặng em cả rừng cây. Nếu đêm dài là tình yêu, anh muốn gửi em cả trời sao lấp lánh.😙 Nhưng trái tim anh không thể dành tặng em được vì nơi đó đã thuộc về em rồi. Chúc em một ngày 8/3 hạnh phúc. ',
     btnYes: 'Yêu anh <33',
     btnNo: 'Iu anh nhìu hơn <33',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
